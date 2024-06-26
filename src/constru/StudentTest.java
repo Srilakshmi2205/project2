059049537c3723cedb80a06558f51e644ab0b245
srila <srilakshmiannamdasu@gmail.com>
2024-06-26 11:26:21 +0530

    StudentTest

---
+++ src/constru/StudentTest.java
@@ -0,0 +1,8 @@
+package constru;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        Student student = new Student("Sri",12);
+        System.out.println(student.getClass());
+    }
+}
